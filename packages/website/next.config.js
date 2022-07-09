0a3c1696da7bc9414ac79194db287c66792d70b7
kim dawoon <dwkim1@greenlabs.co.kr>
2022-07-09 14:56:07 +0900

    최초

---
+++ packages/website/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const withTM = require("next-transpile-modules")(["components"]);
+const withPlugins = require("next-compose-plugins");
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = withPlugins([withTM], nextConfig);
