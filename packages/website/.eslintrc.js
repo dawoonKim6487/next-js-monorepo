5ba4f17d92881c2b7ed3e3e1d8acd552db977799
kim dawoon <dwkim1@greenlabs.co.kr>
2022-07-11 15:19:33 +0900

    next lint
    Signed-off-by: kim dawoon <dwkim1@greenlabs.co.kr>

---
+++ packages/website/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': ['off'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
