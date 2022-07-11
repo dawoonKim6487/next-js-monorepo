0a86c6d1eef55d787ec6c6c2495f330252706517
kim dawoon <dwkim1@greenlabs.co.kr>
2022-07-11 13:36:04 +0900

    components 폴더 구조 변경
    Signed-off-by: kim dawoon <dwkim1@greenlabs.co.kr>

---
+++ packages/components/src/Title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./index.css";
+
+const Title = ({ children }) => {
+  return <h1 className="text-3xl font-bold underline">{children}</h1>;
+};
+
+export default Title;
